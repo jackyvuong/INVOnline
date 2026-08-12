c610457161b94f807a17bd6eb61458fad13e0955
vuong0779 <vuong0779@gmail.com>
2026-08-12 12:35:15 +0700

    init

--- api/obj/Debug/net10.0/Inventory.Api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/Inventory.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64dc4eb86f22fd7c58e45947040f037afa904b41")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
